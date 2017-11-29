607822d800748e751fc97cd3cf62f43f1da873ef
Vincent Pakson <jnpakson@gmail.com>
2017-11-29 19:03:24 +0800

    Add platform labeller route and hbs file

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var newsCtrl = require('../controller/news-controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   newsCtrl.getAllSegment().then(function(queryResult){
+    // console.log(baseUrl);
     console.log(queryResult['unlabelled'].length)
     res.render('index',queryResult);
   }).catch(function(queryError){
@@ -20,6 +21,12 @@ router.get('/ajax-call', function(req, res, next){
   }).catch(function(queryError){
     console.log(queryError.toString());
   });
+});
+
+router.get('/platform',function(req,res,next){
+  // console.log(router.stack)
+  sample = {'item':'blah'};
+  res.render('platform',sample);
 });
 
 module.exports = router;
